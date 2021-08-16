a55b5f6b1658a377728191e18d7b7b12982b5874
MURAD10000 <74201005+MURAD10000@users.noreply.github.com>
2021-08-16 14:33:05 +0600

    Update DSU (Disjoint Set Union).cpp
    From Billah Sir (Cuet Dsu)

--- DSU (Disjoint Set Union).cpp
+++ DSU (Disjoint Set Union).cpp
@@ -1,48 +1,23 @@
-struct DSU
-{
-	int connected;
-	vector<int> par, sz;
-
-	void init(int n) 
-	{
-		par = sz = vector<int> (n + 1, 0);
-		for(int i = 1; i <= n; i++)
-			par[i] = i, sz[i] = 1;
-		connected = n;
-	}
-
-	int getPar(int u)
-	{
-		while(u != par[u])
-		{
-			par[u] = par[par[u]];
-			u = par[u];
-		}
-		return u;
-	}
-
-	int getSize(int u)
-	{
-		return sz[getPar(u)];
-	}
-
-	void unite(int u, int v)
-	{
-		int par1 = getPar(u), par2 = getPar(v);
-
-		if(par1 == par2)
-			return;
-
-		connected--;
-
-		if(sz[par1] > sz[par2])
-			swap(par1, par2);
-
-		sz[par2] += sz[par1];
-		sz[par1] = 0;
-		par[par1] = par[par2];
-	}
+class DSU{
+  vector<int> parent;
+ public:
+  DSU(){}
+  DSU(int no_of_nodes){
+    parent.resize(no_of_nodes+1);
+    for(int i=0; i<=no_of_nodes; ++i) parent[i]=i;
+  }
+  int find_parent(int node){
+    if(node!=parent[node]) parent[node]=find_parent(parent[node]);
+    return parent[node];
+  }
+  void merge_node(int u, int v){
+    int par_u=find_parent(u), par_v=find_parent(v);
+    parent[par_u]=par_v;
+  }
 };
+
+// DSU d1(n), d2(n);
+
 
 //Problem 1 (DSU + Divide and Conquer): https://codeforces.com/contest/813/problem/F
 //Solution 1: https://codeforces.com/contest/813/submission/48548930
